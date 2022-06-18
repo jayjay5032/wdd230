4f664ac36c33253963e0a9d4f5096f0c17016789
jayjay5032 <jayjay5032@gmail.com>
2022-06-17 23:47:10 -0600

    finished w09

---
+++ chamber/scripts/fetch_directory.js
@@ -0,0 +1,43 @@
+const requestURL = 'scripts/data.JSON';
+const grid = document.querySelector('.grid');
+
+function displaybusniess(busniess) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let img = document.createElement('img');
+    let url = document.createElement('a');
+    let name = document.createElement('h3');
+    let address = document.createElement('p');
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    img.setAttribute('src', busniess.imageurl);
+    img.setAttribute('alt', `logo of ${busniess.name}`);
+    img.setAttribute('loading', 'lazy');
+    
+    // name, address and url of the busniess
+    name.textContent = `${busniess.name}`;
+    address.textContent = `${busniess.address}`;
+    var linkText = document.createTextNode("Details");
+    url.appendChild(linkText);
+    url.title = "Details";
+    url.href = `${busniess.url}`;
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(url);
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    grid.appendChild(card);
+}
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    
+    .then(function (jsonObject) {
+        const busniess = jsonObject['busniess'];
+        busniess.forEach(displaybusniess);
+    });
--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -28,7 +28,7 @@ function displayProphets(prophet) {
     card.appendChild(portrait);
 
     // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
+    cards.appendChild(card);
 }
 
 fetch(requestURL)
