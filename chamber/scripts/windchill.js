62a1656d327261d2ce69d945b11fe10d87e0d6ac
jayjay5032 <jayjay5032@gmail.com>
2022-06-24 00:19:00 -0600

    fixed windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,14 @@
-const temperature = document.getElementById("temp");
-const wind_speed = document.getElementById("wind");
+//const temperature = document.getElementById("temp");
+//const wind_speed = document.getElementById("wind");
 var wind_chill = document.getElementById("wind_chill");
-//const temperature = 51;
-//const wind_speed = 2
+const temperature = 49;
+const wind_speed = 5
 
 
-if (temperature < 50 || wind_speed > 3) {
-    wind_chill.textContent = "N/A!!";
+if (temperature <= 50 && wind_speed > 3) {
+    let wind_chill_math = (35.74 + (0.6215 * temperature))-(35.75 * Math.pow(wind_speed,0.16)) + (0.4275*temperature*Math.pow(wind_speed,0.16));   
+    let wind_chilltemp = Math.round((wind_chill_math + Number.EPSILON)*10)/100;
+    wind_chill.textContent = wind_chilltemp;
 } else {
-    wind_chill.textContent = (35.74 + (0.6215 * temperature))-(35.75 * Math.pow(wind_speed,0.16)) + (0.4275*temperature*Math.pow(wind_speed,0.16));
+    wind_chill.innerHTML = "N/A!!";
 }
